832cef0e858534e08d0df82501cb5aa8ad086dfe
twiztd1166 <200838923+twiztd1166@users.noreply.github.com>
2026-08-14 23:07:58 -0400

    Bump field cache to v3.11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pcm-field-v3-10-2026-08-14';
+const CACHE='pcm-field-v3-11-2026-08-14';
 const CORE=['./','./index.html','./style.css','./style-v2.css','./pwa-v3.css','./hardening-v3-2.css','./plain-data.js','./provenance-v3-2.js','./core-v2.js','./lookup-v2.js','./browse-v3.js','./release-v2a.js','./history-v2.js','./release-v2b.js','./pwa-v3.js','./field-v36.js','./field-v37.js','./boot-v2.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('pcm-field-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
